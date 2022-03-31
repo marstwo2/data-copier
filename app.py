16aa2c5c64e09a939dce3c4c6def911ada558b0a
marstwo2 <marstw05@aol.com>
2022-03-31 22:13:35 +0000

    'update

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
+import os
+import pandas as pd
+
+BASE_DIR = '/home/jshernandez05/Projects/data-copier/data/retail_db_json'
+table_name = 'order_items'
+file_name = os.listdir(f'{BASE_DIR}/{table_name}')[0]
+fp = f'{BASE_DIR}/{table_name}/{file_name}'
+
+conn = 'postgresql://retail_user:itversity@localhost:5452/retail_db'
+
 def main():
-    print("Hello World! from jshernandez05")
+    df = pd.read_json(fp, lines=True)
+    print(df.dtypes)
 
 if __name__ == "__main__":
     main()
