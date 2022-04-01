ea7d68047a10e12f01b5029bdfd40d77bf6fa923
marstwo2 <marstw05@aol.com>
2022-04-01 22:26:05 +0000

    Updated all file, created .env

--- app.py
+++ app.py
@@ -1,10 +1,31 @@
 import os
+from read import get_json_reader
+from write import load_db_table
 import sys
 
-import pandas as pd
+from dotenv import find_dotenv, load_dotenv
 
-print(sys.executable)
-print('test 2')
+def process_tables(BASE_DIR, conn, table_name):
+    json_reader = get_json_reader(BASE_DIR, table_name)
+    for df in (json_reader):
+        load_db_table(df, conn, table_name, df.columns[0])
+
+def main():
+    load_dotenv(find_dotenv())
+    BASE_DIR = os.environ.get('BASE_DIR')
+    table_names = sys.argv[1].split(' ')
+    configs = dict(os.environ.items())
+    conn = f'postgresql://{configs["DB_USER"]}:{configs["DB_PASS"]}@{configs["DB_HOST"]}:{configs["DB_PORT"]}/{configs["DB_NAME"]}'
+    for table_name in table_names:
+        process_tables(BASE_DIR, conn, table_name)
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+
 # def get_json_reader(BASE_DIR, table_name, chunksize=1000):
 #     file_name = os.listdir(f'{BASE_DIR}/{table_name}')[0]
 #     fp = f'{BASE_DIR}/{table_name}/{file_name}'
