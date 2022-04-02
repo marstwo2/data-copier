5b301c0003150613f859a58da8a26e933417dede
marstwo2 <marstw05@aol.com>
2022-04-02 07:46:54 +0000

    updated dockercommand.sh with `pwd`

--- dockercommand.sh
+++ dockercommand.sh
@@ -3,12 +3,12 @@ docker run \
   --name data-copier \
   --rm \
   --network data-copier-nw \
-  -v /home/jshernandez05/Projects/data-copier/data/retail_db_json:/data-copier/data/retail_db_json \
+  -v `pwd`/data/retail_db_json:/data-copier/data/retail_db_json \
   -e BASE_DIR=/data-copier/data/retail_db_json \
   -e DB_PASS=itversity \
   -e DB_USER=retail_user \
   -e DB_HOST=a936820710aa \
   -e DB_PORT=5432 \
   -e DB_NAME=retail_db \
-  data-copier python /data-copier/app/app.py "departments categories"
+  data-copier python /data-copier/app/app.py "departments categories products orders order_items customers"
 
